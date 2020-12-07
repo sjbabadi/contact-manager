9a927762fbccb7e568d44e7a33abb00d998a3a9d
sjbabadi <sheila.babadi@yahoo.com>
2020-12-06 20:20:48 -0500

    Added rest of DataStore CRUD methods

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,5 +9,28 @@
 
   const cm = new ContactManager();
 
+  // cm.storage.get(3);
+  // cm.storage.getAll();
+  // cm.storage.remove(3);
+  // cm.storage.getAll();
+
+  let newContact = {
+    "full_name": "Sheila Babadi",
+    "email": "sheila.babadi@yahoo.com",
+    "phone_number": "123-4567",
+    "tags": "personal, family"
+  };
+
+  cm.storage.add(newContact);
+  cm.storage.getAll();
+
+  let updatedContact = {
+    "full_name": "Updated Babadi",
+    "email": "sheila.babadi@yahoo.com",
+    "phone_number": "123-4567",
+    "tags": "personal, family"
+  };
+
+  cm.storage.update(3, updatedContact);
   cm.storage.getAll();
 })();
--- public/javascripts/datastore.js
+++ public/javascripts/datastore.js
@@ -8,14 +8,79 @@
   }
 
   DataStore.prototype.get = function(id) {
-
+    fetch(`${SERVER_URL}/${id}`)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('Request was not successful');
+      } else {
+        return res.json();
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
   };
 
   DataStore.prototype.getAll = function() {
-    fetch(SERVER_URL)
-    .then(res => res.json())
+    fetch(`${SERVER_URL}`)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('Request was not successful');
+      } else {
+        return res.json();
+      }
+    })
     .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+    .catch(error => console.error(error));
+  };
+
+  DataStore.prototype.remove = function(id) {
+    fetch(`${SERVER_URL}/${id}`, {
+      method: 'DELETE'
+    }).then(res => {
+      if(!res.ok) {
+        throw new Error('Contact not found');
+      } else {
+        console.log(`Contact with id ${id} was successfully removed`);
+      }
+    }).catch(error => console.log(error));
+  };
+
+  DataStore.prototype.add = function(contact) {
+    fetch(`${SERVER_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(contact)
+    })
+    .then(res => {
+     if(!res.ok) {
+        throw new Error('Unable to create contact');
+      } else {
+        res.json();
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+  };
+
+  DataStore.prototype.update = function(id, contact) {
+    fetch(`${SERVER_URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(contact)
+    })
+    .then(res => {
+     if(!res.ok) {
+        throw new Error('Unable to update contact');
+      } else {
+        res.json();
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
   };
 
   window.app = window.app || {};
